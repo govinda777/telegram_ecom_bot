877a26cbc4a98663ae41982284f2b2bffd45de9f
Gov <govinda777@gmail.com>
2024-08-04 21:25:48 -0300

    Exec act in local

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # telegram_ecom_bot
 telegram_ecommerce_bot
+
+
+## Run Pipe in local
+
+brew install act
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-    entry: './app.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        new Dotenv()
-    ]
+  entry: './app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new Dotenv()
+  ],
+  mode: 'production'
 };
